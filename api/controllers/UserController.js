bbfded5ff260679044d78956e53005196f1a78d0
setu_modi <setu@intricare.net>
2016-10-07 19:24:55 +0530

    associate user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,12 +1,12 @@
 module.exports = {
 
 	List: function(req,res) {
-		Associate_user.find().exec(function(err,result){
+		User.find().where({'parent_id':req.session.userid})	.exec(function(err,result){
 			if(err){ 
 				console.log(err);
 			}else{   
-				var data;
-				data = result;
+				var data={};
+				data.list = result;
 				console.log(data);
 				res.locals.layout = 'layout1.ejs';
 		        return res.view('User/list.ejs',data);
@@ -22,9 +22,11 @@ module.exports = {
 
 	createUser: function(req,res) {	
 		var form_data = req.body;
-		Associate_user.create(form_data, function(err, auth) {
-          if(err){   console.log(err); }else {  console.log('successfully inserted'); }
-        });		
-		res.redirect('/users');
+		console.log(form_data);
+		form_data.parent_id = req.session.userid;
+		User.create(form_data, function(err, auth) {
+          if(err){ console.log(err); }else {  console.log('successfully inserted'); }
+        });
+        res.redirect('/users');
 	},
 }
